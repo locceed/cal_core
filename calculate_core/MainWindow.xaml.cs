95b7f632fe0028d16807fa5f492281c755311b27
locceed <1403306849@qq.com>
2016-10-05 20:45:38 +0800

    ing

--- calculate_core/MainWindow.xaml.cs
+++ calculate_core/MainWindow.xaml.cs
@@ -32,6 +32,7 @@ namespace calculate_core
         }
         private void button1_Click(object sender, RoutedEventArgs e)
         {
+            output.Text = other.use1(input.Text);
         }
     }
 }
--- calculate_core/other.cs
+++ calculate_core/other.cs
@@ -13,6 +13,29 @@ namespace calculate_core
         static public string bracket1(string input)//括号（ing）
         {
 
+            if (input.First().ToString().IndexOfAny("+-".ToArray()) == -1)//统一格式
+            {
+                input = "+" + input;
+            }
+
+            return "";
+        }
+        static public string use1(string input)//单括号计算
+        {
+            string part_op = input.First().ToString();
+            string part_cal = Cal.cal3(input.Substring(2, input.Length - 3));
+            return part_op + part_cal;
+        }
+        static public string use2(string input)//分解多括号
+        {
+            if (input.IndexOf("(") == -1)
+            {
+                return (-1).ToString();
+            }
+            else
+            {
+
+            }
             return "";
         }
     }
