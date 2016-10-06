b4c596e661d29e6354a8015afaa7eae7ea5afc45
locceed <1403306849@qq.com>
2016-10-06 10:29:56 +0800

    艹

--- calculate_core/other.cs
+++ calculate_core/other.cs
@@ -22,52 +22,40 @@ namespace calculate_core
         }
         static public string use1(string input)//单括号计算
         {
-            string part_op = input.First().ToString();
-            string part_cal = Cal.cal3(input.Substring(2, input.Length - 3));
+            if (input.First().ToString().IndexOfAny("+-".ToArray()) == -1)//统一格式
+            {
+                input = "+" + input;
+            }
+            string part_op = "";
+            string part_cal = "";
+            part_op = input.First().ToString();
+            part_cal = Cal.cal3(input.Substring(2, input.Length - 3));
+            if (part_cal.IndexOf("error") != -1)
+            {
+                return "error";
+            }
             return part_op + part_cal;
         }
         static public string use2(string input)//分解多括号
         {
-            if (input.IndexOf("(") == -1)
+            string[] part = new string[Convert.ToInt32(Math.Pow(input.Length,2))];
+            int x1 = 0 ;
+            for (int for1 = 0; for1 <= input.Length; for1++) 
             {
-                return "-1";
-            }
-            else
-            {
-                int x1 = 0;
-                int x2 = 0;
-                int x3 = 0;
-                int x4 = 0;
-                int x5 = 0;
-                string[] bracket1 = new string[1024];
-                while (true)
+                for (int for2 = input.Length; for2 >= 0; for2--) 
                 {
-                    if (x1 == -1)
+                    try
                     {
-                        break;
+                        part[x1]= use1(input.Substring(for1, for2));
+                        x1++;
                     }
-                    else
+                    catch
                     {
-                        while (true) 
-                        {
-                            x1 = input.IndexOf("(", x1 + 1);
-                            x4++;
-                            x3 = input.IndexOf(")", x1 + 1);
-                            if (x2 != x3)
-                            {
-                                x4--;
-                            }
-                            x3 = x2;
-                            if (x4 == 0)
-                            {
-                                break;
-                            }
-                        }
+                        x1++;
                     }
-
                 }
-                return "";
             }
+            return "";
         }
     }
 }
